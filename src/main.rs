3e2b6314e341f54bca1a4e1b4b6125d517135878
Eoin Halligan <ehalligan12@gmail.com>
2019-08-24 15:07:54 -0400

    reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use std::io;
+use std::env;
+// use the file system
+use std::fs;
+
 // function that accepts a file and returns a list of tokens
 fn main() {
     println!("Begining...");
-
+    readFromFile("./etc/return_2.c");
 }
 
+fn readFromFile(path: &str) {
+    let contents = fs::read_to_string(path).expect("Whoops");
+    println!("With text:\n{}", contents);
+
+}
